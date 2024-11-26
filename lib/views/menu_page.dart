83aacffff55183bf404d773f58e3ae1a58504e49
Rayyanda <rayyanda5113@gmail.com>
2024-11-26 07:26:46 +0700

    menambahkan keterangan pada menuPage

--- lib/views/menu_page.dart
+++ lib/views/menu_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
 class MenuPage extends StatelessWidget{
+
+  //membuat permintaan sebagai parameter
   final String title;
   final IconData icons;
   final List<String> imgs;
   
+  //mendefinisikan bahwa permintaan/parameter harus dimasukkan ketika ingin menggunakan Widget/halaman ini
   const MenuPage({
    super.key,
    required this.title,
@@ -15,21 +18,41 @@ class MenuPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //appbar
       appBar: AppBar(
+
+        //mengatur backgorund appBar
         backgroundColor: Colors.lightBlueAccent,
+
+        //mengatur foreground (warna teks) appBar
         foregroundColor: Colors.white,
+
+        //mengatur tulisan di AppBar yang dibungkus dengan widget Row supaya urutan widget horizontal
         title: Row(
+
+          //mengatur alignment supaya rata kiri
           mainAxisAlignment: MainAxisAlignment.start,
+
+          //mengisi Row
           children: [
+
+            //icons
             Icon(icons),
+
+            //text pada AppBar berupa title
             Text(title),
           ]
         ),
       ),
+      //membungkus body dengan ListView supaya menjadi satu halaman yang disusun secara vertikal
       body: ListView(
         children: <Widget>[
+
+          //membuat padding / jarak terhadap ukuran layar
           Padding(
             padding: const EdgeInsets.all(16.0),
+
+            //membuat komponen berupa grid supaya tersusun secara baris dan kolom
             child:  GridView.builder(
             shrinkWrap: true,
             physics: const NeverScrollableScrollPhysics(),
@@ -46,7 +69,11 @@ class MenuPage extends StatelessWidget{
             }
           ),
           ),
+
+          //mengatur jarak antara gambar dengan tombol kembali
           const SizedBox(height: 20),
+
+          //tombol/button untuk kembali ke halaman awal
           ElevatedButton(
               style: ButtonStyle(
                 backgroundColor: MaterialStateProperty.all(Colors.blue),
